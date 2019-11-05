af742556699d5d39a7454110ff37f378ce442985
Vince Lollino <charlie_fog@comcast.net>
2019-11-05 12:54:26 -0600

    subscribed to URL to update dates

--- src/app/apod/apod.component.ts
+++ src/app/apod/apod.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+//1. Import ActivatedRoute
+import { ActivatedRoute } from '@angular/router';
 import { ApodService } from '../api/apod.service';
 import { Apod } from '../models/apod';
 
@@ -29,23 +30,39 @@ export class ApodComponent implements OnInit {
 
   apod: Apod;
 
-  constructor(private apodService: ApodService) { }
+  constructor(
+    private apodService: ApodService,
+    //2. Inject ActivatedRoute into the constructor
+    private router: ActivatedRoute) { }
 
   ngOnInit() {
-    this.getApod();
+        //3. Subscribe to parameterized route
+        this.router.params.subscribe((params) => {
+          this.getApod(params['date']);
+    // this.getApod();
     // console.log(this.apodService.getApod());
     // this.apodService.getApod().subscribe(
     //   (response: any) => {
     //     console.log(response);
-  }
+  });
+}
 
-  getApod(): void {
+  // getApod(): void {
     // added after updted apod.service.ts with date infor in getApod//
-    let date = new Date().toISOString().slice(0.10);
+    // let date = new Date().toISOString().slice(0.10);
+
+    // this.apodService.getApod(date).subscribe(
+    //   (response: any) => {
+    //     this.apod = response;
+      //4. Replace the current date with an updated method signature
+  getApod(date:string): void{
 
     this.apodService.getApod(date).subscribe(
-      (response: any) => {
+      (response:any)=>{
         this.apod = response;
+
+        //5. Log the results to the JS console
+        console.log(response);
       }
     );
   }
