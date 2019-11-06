85d3f854126b3313ea16238a9d9b7588fc6f9fbe
Vince Lollino <charlie_fog@comcast.net>
2019-11-05 19:13:43 -0600

    Got deep links working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 //Import HttpClientModule
 import { HttpClientModule } from '@angular/common/http';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { NgApodConfig } from '../../config/ng-apod.config';
     SafePipeModule
   ],
   providers: [
-    NgApodConfig //Add as a provider
+    NgApodConfig,
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ],
   bootstrap: [AppComponent]
 })
